fed6611d22976589c5c642bc767a631de1b250a7
unknown <pv95335@gmail.com>
2020-07-06 20:43:02 -0700

    User input giving response

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,50 +3,94 @@
 // GoogleBooks Api Work
 
 var authorInput = document.querySelector("#AuthorInput");
-var titleInput = document.querySelector("#TitleInput");
+var titleInput = document.getElementById("TitleInput")
 var genreInput = document.querySelector("#GenreInput");
+
+/* let searchOptions = [authorInput.value, titleInput.value, genreInput.value]
+let searchFlags = []
+searchOptions.forEach(type => {
+    if (type.length >= 0) {
+        searchFlags.push(type)
+    }
+}) */
 
 
 
 // -------Function to fetch Book Information Start-------
 
 
-//var getBooks = function(response) {
-
+var getBooks = function(titleResponse) {
+var bookImg;
 // Variable for GoogleBooks API
-var googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyD2rj-uQGtYypreqRXJvRLLVg2sCgjhE2s"
+var googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + titleResponse + "&key=AIzaSyCRSXdaLKLF0hPkiN03bDL9-swkrelDh8w"
 
 // Fetching GoogleBooks API
 fetch(googleApiUrl)
-.then(function() {
+.then(function(response) {
     if(response.ok) {
-        response.json().then(function(){
-            displayBook();
-            console.log(googleApiUrl)
+        response.json().then(function(data){
+            console.log(data)
+            data.items.forEach(book => {
+                let title = book.volumeInfo.title
+                let author = book.volumeInfo.authors
+                bookImg = book.volumeInfo.imageLinks.thumbnail
+                
+                let newBook = {
+                    title: title,
+                    author: author,
+                    image: bookImg
+                }
+                console.log(newBook)
+            })
+            
+            //displayBook(data,titleResponse);
+           //newbooks[Matth.floor(Math.random * newbooks.length)]
         });
     } else {
         alert("Error: ");
     }
 })
-.catch(function(error) { 
-    alert("Unable to connect")
-})
 
-    //console.log(googleApiUrl);
-
-
-//$(document).on("click", "#btn_btn-warning", function(event){
-    //alert("GO");
-//});
-//};
-//for (var i=0; i<.length;i=0)
-//});
+    console.log(googleApiUrl);
+}
 // -------Function to fetch Book Information End-------
 
+console.log(titleInput);
 
+//var titleInput = document.querySelector("#booksCrit")
+
+console.log(formSubmitHandler)
+
+//titleInput = addEventListener("submit", formSubmitHandler)
+// get the button
+let searchBtn = document.querySelector('#searchBtn')
+searchBtn.addEventListener('click', (event)=> {
+    let searchTerm = titleInput.value
+    event.preventDefault()
+    if (searchTerm.includes(" ")) {
+        searchTerm = searchTerm.split(" ").join("+")
+        console.log(searchTerm)
+    }
+
+    // call on the getBooks to make API request
+    getBooks(searchTerm)
+    
+})
+
+function formSubmitHandler (event) {
+    event.preventDefault();
+
+    if (titleInput.value) {
+        //getBookInfo(titleInput.value);
+        //titleInput.value="";
+    } else {
+        //alert("Please enter a book title");
+    }
+    console.log(event)
+}
 
 console.log()
 
 //bookDetails.appendChild(bookImage)
 
-//getBooks()
+getBooks()
