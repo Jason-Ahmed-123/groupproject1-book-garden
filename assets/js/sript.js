e25dda8431171d00bf04d72f2e0aa112a7f7c685
Windy <lauren.euge@gmail.com>
2020-07-06 18:12:21 -0700

    book description

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,10 +1,13 @@
 //OPEN LIBRARY API//
-var authorInput = document.querySelector("AuthorInput");
+var displayBooks = document.querySelector("#cardContainment")
 var titleInput = document.querySelector("TitleInput");
 var genreInput = document.querySelecetor("GenreInput");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var displayBooks = document.querySelector("#cardContainment")
 
-var openLibraryApiUrl = function(user) {
+
+var getBookInfo = function(titleInput) {
+
+var openLibraryApiUrl = function(authorInput) {
     // format the github api url
     var openLibraryApiUrl = "http://openlibrary.org/query.json?type=/type/edition&authors=/authors/OL1A" + authorInput + "/search";
 
@@ -25,36 +28,39 @@ var openLibraryApiUrl = function(user) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to find author");
       });
-    };
+    }
+  };
 
 //create form variable for form submit handler//
-//var authorInput//
+var authorInput = document.querySelector("AuthorInput");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // var for form submit (replace username)//
     var authorInput = authorInputEl.value.trim();
       
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
+    if (authorInput) {
+        getBookInfo(authorInput.value);
+        authorInput.value = "";
     } else {
-    alert("Please enter title/author/genre");
+    alert("Please enter author");
     }
         console.log(event);
     }; 
 
     //add event listener for form submit//
+authorInput = addEventListener("submit", formSubmitHandler)
 
     //add append child?//
+bookContainer.appendChild(displayBooks);
 
-var getFeaturedRepos = function(language) {
-    var apiUrl = "openlibrary/plugins/openlibrary/js?q=" + language + "+is:featured&sort=help-wanted-issues";
+var bookDescription = function(books, authorInput) {
+    var openLibraryApiUrl = "openlibrary/plugins/openlibrary/js?q=" + author + "+is:featured&sort=help-wanted-issues";
       
         fetch(apiUrl).then(function(response) {
           if (response.ok) {
             response.json().then(function(data) {
-              displayRepos(data.items, language);
+              displayRepos(data.items, author);
             });
           } else {
             alert("Error: " + response.statusText);
