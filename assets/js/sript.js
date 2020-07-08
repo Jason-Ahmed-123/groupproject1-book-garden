adcf2b1d3ad0915a13c2e9536ddcc21390e48a08
Windy <lauren.euge@gmail.com>
2020-07-07 18:10:52 -0700

    added search btn

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -1,4 +1,5 @@
 //OPEN LIBRARY API//
+
 var authorInput = document.querySelector("AuthorInput");
 var titleInput = document.querySelector("TitleInput");
 var genreInput = document.querySelecetor("GenreInput");
@@ -6,15 +7,30 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var openLibraryApiUrl = function(user) {
     // format the github api url
-    var openLibraryApiUrl = "http://openlibrary.org/query.json?type=/type/edition&authors=/authors/OL1A" + authorInput + "/search";
-
+    var openLibraryApiUrl = "http://openlibrary.org/api/books?bibkeys=" + authorInput + "ISBN:0451526538&callback=mycallback";
+    
+var getBooks = function(titleResponse) {
+  var bookImg;
     // make a request to the url
     fetch(openLibraryApiUrl)
       .then(function(response) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
+            console.log(data)
+            data.items.forEach(book => {
+              let author = book.//name from api//.authors
+              bookImg = book.//add api info//
+
+              let newBook = {
+                title: title,
+                author: author, 
+                image: bookImg
+              }
+              console.log(newBook)
+            })
             displayAuthor(data, author);
+
               console.log(openLibraryApiUrl)
           });
         } else {
@@ -25,14 +41,36 @@ var openLibraryApiUrl = function(user) {
         // Notice this `.catch()` getting chained onto the end of the `.then()` method
         alert("Unable to find author");
       });
+
     };
 
 //create form variable for form submot handler//
+
+console.log(authorInput);
+
+//search btn//
+
+let searchBtn = document.querySelector('#searchBtn')
+searchBtn.addEventListener('click', (event)=> {
+    let searchTerm = authorInput.value
+    event.preventDefault()
+    if (searchTerm.includes(" ")) {
+        searchTerm = searchTerm.split(" ").join("+")
+        console.log(searchTerm)
+    }
+
+    // call on the getBooks to make API request
+    getBooks(searchTerm)
+    
+})
+//end btn//
+
 
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // var for form submit (replace username)//
+
     var username = nameInputEl.value.trim();
       
     if (username) {
@@ -40,21 +78,27 @@ var formSubmitHandler = function(event) {
         nameInputEl.value = "";
     } else {
     alert("Please enter title/author/genre");
+
     }
         console.log(event);
     }; 
 
+    console.log(formSubmitHandler)
+
     //add event listener for form submit//
 
-    //add append child?//
 
-var getFeaturedRepos = function(language) {
+    //add append child?//
+/*var getFeaturedRepos = function(language) {
     var apiUrl = "openlibrary/plugins/openlibrary/js?q=" + language + "+is:featured&sort=help-wanted-issues";
+
       
         fetch(apiUrl).then(function(response) {
           if (response.ok) {
             response.json().then(function(data) {
+
               displayRepos(data.items, language);
+
             });
           } else {
             alert("Error: " + response.statusText);
@@ -72,6 +116,7 @@ var buttonClickHandler = function(event) {
           repoContainerEl.textContent = "";
         }
         console.log(language);
+
       };
       
-languageButtonsEl.addEventListener("click", buttonClickHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);*/
